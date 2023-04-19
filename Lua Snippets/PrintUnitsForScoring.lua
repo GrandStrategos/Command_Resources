1f99478d3c4afa99b9e7077110a0ca7d3de884de
blu3ser <92254994+blu3ser@users.noreply.github.com>
2023-04-19 10:06:08 +0200

    Create Print Units for Scoring

---
+++ Lua Snippets/PrintUnitsForScoring.lua
@@ -0,0 +1,23 @@
+-- This functions print in Lua console all the differents units in a side to create a table for a standard UnitDestroyed function.
+function addToSet(set, key)
+    set[key] = true
+end
+
+function removeFromSet(set, key)
+    set[key] = nil
+end
+
+function setContains(set, key)
+    return set[key] ~= nil
+end
+
+local side = "US" --CHANGE THIS TO THE NAME OF THE SIDE YOU WANT THE UNITS
+local base = {}
+local s = VP_GetSide({ name = side})
+for k, v in pairs(s.units) do
+    local unit = ScenEdit_GetUnit({ guid = v.guid })
+    if unit.type ~= "Group" and not setContains(base, unit.dbid) then
+        print("{type= '"..unit.type.."', class= '"..unit.classname.."',name='"..unit.name.. "',dbid= "..unit.dbid..", points=0, descriptor='destroyed'},")
+        addToSet(base,unit.dbid)
+    end
+end
